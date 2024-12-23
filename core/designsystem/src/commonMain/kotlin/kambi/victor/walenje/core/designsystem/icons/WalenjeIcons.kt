472c0a3aced11ee3056c92fdafb3f099e6bf3fbc
Kambi-V <erratickambi@gmail.com>
2024-12-24 00:07:50 +0300

    feat(design-system): add icons

--- core/designsystem/src/commonMain/kotlin/kambi/victor/walenje/core/designsystem/icons/WalenjeIcons.kt
+++ core/designsystem/src/commonMain/kotlin/kambi/victor/walenje/core/designsystem/icons/WalenjeIcons.kt
@@ -1,4 +1,19 @@
 package kambi.victor.walenje.core.designsystem.icons
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowLeft
+import androidx.compose.material.icons.filled.ArrowLeft
+import androidx.compose.material.icons.filled.ChevronLeft
+import walenjeapp.core.designsystem.generated.resources.Res
+import walenjeapp.core.designsystem.generated.resources.gravity_ui_arrow_chevron_right
+import walenjeapp.core.designsystem.generated.resources.mdi_circle
+import walenjeapp.core.designsystem.generated.resources.mdi_circle_outline
+import walenjeapp.core.designsystem.generated.resources.ri_arrow_left_long_line
+
 object WalenjeIcons {
+  val ArrowChevronRight = Res.drawable.gravity_ui_arrow_chevron_right
+  val Circle = Res.drawable.mdi_circle
+  val CircleOutline = Res.drawable.mdi_circle_outline
+  val ArrowLeftLong = Res.drawable.ri_arrow_left_long_line
+  val ArrowLeft = Icons.Default.ChevronLeft
 }
