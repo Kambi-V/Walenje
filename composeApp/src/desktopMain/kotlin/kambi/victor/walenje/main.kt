9a3d1a2d44cae65329ac9515b1c49f01a355f607
Kambi-V <erratickambi@gmail.com>
2025-02-21 17:25:32 +0300

    fix(desktop): set temporary fix for biometrics to always pass in desktop
    Signed-off-by: Kambi-V <erratickambi@gmail.com>

--- composeApp/src/desktopMain/kotlin/kambi/victor/walenje/main.kt
+++ composeApp/src/desktopMain/kotlin/kambi/victor/walenje/main.kt
@@ -2,7 +2,11 @@ package kambi.victor.walenje
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kambi.victor.walenje.core.authentication.BiometricPromptManager
+
+val manager = BiometricPromptManager()
+private val biometrics by lazy { BiometricPromptManager() }
 
 fun main() = application {
-  Window(onCloseRequest = ::exitApplication, title = "WalenjeApp") { App() }
+  Window(onCloseRequest = ::exitApplication, title = "WalenjeApp") { App(biometrics) }
 }
--- feature/home/src/commonMain/kotlin/kambi/victor/walenje/feature/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/kambi/victor/walenje/feature/home/HomeScreen.kt
@@ -2,13 +2,17 @@ package kambi.victor.walenje.feature.home
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import kambi.victor.walenje.core.designsystem.NavItem
 import kambi.victor.walenje.core.designsystem.WalenjeNavigationBar
 import kambi.victor.walenje.core.designsystem.icons.WalenjeIcons
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(onNavigateBack: () -> Unit) {
   val navigationItems =
@@ -18,7 +22,10 @@ fun HomeScreen(onNavigateBack: () -> Unit) {
       3 to NavItem("Profile", WalenjeIcons.Profile),
     )
 
-  Scaffold(bottomBar = { WalenjeNavigationBar(navigationItems, 1) }) { innerPadding ->
+  Scaffold(
+    topBar = { TopAppBar(title = { Text("Home Screen") }) },
+    bottomBar = { WalenjeNavigationBar(navigationItems, 1) },
+  ) { innerPadding ->
     LazyColumn(modifier = Modifier.padding(innerPadding)) {
       item {}
       item {}
